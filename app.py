3d84ea6e988d5c316a1043c36f783435ba7940a2
hjmr <2620990+hjmr@users.noreply.github.com>
2020-05-12 01:31:04 +0900

    add pylint and autopep

--- app.py
+++ app.py
@@ -1,23 +1,32 @@
 import os
+import random
 from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
+urls = ["kaleido", "sakura", "snow_fall"]
+
+
 @app.route("/")
 def index():
-    return redirect(url_for("kaleido"))
+    x = int(random.random() * 3)
+    return redirect(url_for(urls[x]))
+
 
 @app.route("/kaleido")
 def kaleido():
     return render_template("kaleido.html")
 
+
 @app.route("/sakura")
 def sakura():
     return render_template("sakura.html")
 
+
 @app.route("/snowfall")
 def snow_fall():
     return render_template("snow_fall.html")
+
 
 if __name__ == "__main__":
     app.run()
